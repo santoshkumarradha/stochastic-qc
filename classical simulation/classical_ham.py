50fda5049d88576c9de105128151b1f424962a49
santoshkumarradha <srr70@case.edu>
2020-11-02 15:20:19 -0500

    added classical algo

--- classical simulation/classical_ham.py
+++ classical simulation/classical_ham.py
@@ -27,64 +27,29 @@ class classical_ham():
         self.m = m
         self.start_sigma = start_sigma
         self.start_mu = start_mu
-        self.P = self.get_p()
         self.ntimes = ntimes
         self.V = None
         self.result = None
         self.initial = None
         self.p0 = start_p0
-
-    def get_p(self):
-        """Momentum operator in momentum basis
-
-        Returns:
-            Unitary: Momentum operator
-        """
-        # H_p=p^2/2m *dt/hbar
-        H_p = self.x**2 / (2*self.m)
-        exp = np.exp(-1j*H_p*self.dt/hbar)
-        p = np.diag(exp)
-        return p
-
-    def get_v(self, divide=True):
-        """Potential operator
-
-        Returns:
-            Unitary: Potenaital operator
-        """
-        # seems like I need to reverse the potential, why??
-        k = 1
-        if divide:
-            k = 2
-        exp = np.exp(-1j*self.v*self.dt/(hbar*k))[::-1]
-        p = np.diag(exp)
-        return p
+        self.V_opp = None
+        self.x0 = gaussian(self.x, self.start_mu, self.start_sigma, self.p0)
+        self.x0 /= np.linalg.norm(self.x0)
+        self.cnt = 0
 
     def set_V(self, v):
-        """Set the potential, use self.x to get the discritized real space
-        example - for quadratic potential, 
-        set_V(a*self.x**2)
-
-        Args:
-            v (array): Array of dimension 2^n
-        """
-        if len(v) != len(self.x):
-            raiseExceptions(
-                "length of v array must be eqaul to x mesh -> (2^n) ")
         self.v = v
-        self.V = self.get_v()
+        self.V_opp = np.diag(np.exp(-1j*self.v * self.dt/2)[::-1])
+        self.p2m = np.diag(np.exp(-1j*self.x**2/(2*self.m) * self.dt))
 
-    def ham_once(self, x):
-        v = self.V
-        p2m = self.get_p()
-        return v@ifft(p2m@fft(v@x))
-
-    def get_result(self, T=1, dt=0.1):
-        self.dt = dt
-        self.ntimes = int(T/dt)
-        x0 = gaussian(self.x, self.start_mu, self.start_sigma, self.p0)
-        x0 /= np.linalg.norm(x0)
-        x = x0
-        for i in range(self.ntimes):
-            x = self.ham_once(x)
+    def simple_v(self, t, dt=None):
+        if dt is not None:
+            self.dt = dt
+        self.set_V(self.v)
+        def applyh(x): return self.V_opp@ifft(self.p2m@fft(self.V_opp@x))
+        x = self.x0
+        t = t * 2**self.n
+        self.cnt = t
+        for _ in range(int(t/self.dt)): # in total does 2^n/(dt) steps between each time steps
+            x = applyh(x)
         return np.real(x*x.conj())
